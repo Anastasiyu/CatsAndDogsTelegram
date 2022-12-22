bf316d32bf740a2bc73a4d0a4635ef0fb624db20
Anastasiyu <asy.safiulina@mail.ru>
2022-12-22 09:24:08 +0300

    Initial commit

---
+++ src/main/java/com/example/catsanddogstelegram/CatsAndDogsTelegramApplication.java
@@ -0,0 +1,13 @@
+package com.example.catsanddogstelegram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatsAndDogsTelegramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatsAndDogsTelegramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/catsanddogstelegram/CatsAndDogsTelegramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.catsanddogstelegram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatsAndDogsTelegramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
