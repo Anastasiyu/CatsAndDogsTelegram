fa48a017ebda22fb2ab043570fa30de58ad19b8b
ChibiaSad <magur1997@gmail.com>
2022-12-24 23:06:25 +0300

    Добавлено меню, реализованы команды start, help, time, address

--- src/main/resources/liquibase/scripts/user-chibiaSad.sql
+++ src/main/resources/liquibase/scripts/user-chibiaSad.sql
@@ -1,1 +1,23 @@
 -- liquibase formatted sql
+-- changeset chibiaSad:1
+CREATE TABLE users
+(
+    chat_id          BIGINT          PRIMARY KEY ,
+    user_name        VARCHAR (100)   NOT NULL,
+    user_age         INT             NOT NULL,
+    user_time        TIMESTAMP       NOT NULL
+);
+
+--changeset chibiaSad:2
+CREATE TABLE animals
+(
+    animal_id        SERIAL          PRIMARY KEY,
+    register_date    TIMESTAMP       NOT NULL,
+    dtype            VARCHAR         NOT NULL,
+    animal_gender    BOOLEAN         NOT NULL,
+    animal_name      VARCHAR(100),
+    animal_age       INT             NOT NULL,
+    description      TEXT,
+    chat_id          BIGINT          REFERENCES users (chat_id)
+);
+
