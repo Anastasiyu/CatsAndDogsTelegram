a02d3b3135ae34228f8f1893adfd4013882126d5
Nataliya <liya15rus@yandex.ru>
2023-01-12 23:51:44 +0300

    добавлены сервисы

---
+++ src/main/java/com/example/catsanddogstelegram/service/CatService.java
@@ -0,0 +1,38 @@
+package com.example.catsanddogstelegram.service;
+
+import com.example.catsanddogstelegram.entity.Cat;
+import com.example.catsanddogstelegram.exception.CatNotFoundException;
+import com.example.catsanddogstelegram.repository.CatRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class CatService {
+    private final CatRepository catRepository;
+
+    public CatService(CatRepository catRepository) {
+        this.catRepository = catRepository;
+    }
+
+    public Cat create(Cat cat) {
+        log.info("method create started");
+        cat.setAnimalId(null);
+        return catRepository.save(cat);
+    }
+
+    public Cat read(long animalId) {
+        log.info("method read started");
+        return catRepository.findById(animalId).orElseThrow(() -> new CatNotFoundException(animalId));
+    }
+
+    public Cat update(long animalId, Cat cat) {
+        log.info("method update started");
+        return catRepository.save(cat);
+    }
+
+    public void delete(long animalId) {
+        log.info("method delete started");
+        catRepository.deleteById(animalId);
+    }
+}
