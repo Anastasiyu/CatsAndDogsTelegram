47e8449171c0dc5590b2030d78dbc269610d183b
ChibiaSad <magur1997@gmail.com>
2023-01-14 16:45:32 +0300

    описан javadoc

--- src/main/java/com/example/catsanddogstelegram/service/AdoptMessageService.java
+++ src/main/java/com/example/catsanddogstelegram/service/AdoptMessageService.java
@@ -34,7 +34,6 @@ public class AdoptMessageService {
     /**
      * Вывод константы {@link PreparingInfoDog#LIST_OF_DOCUMENTS_TO_ADOPT}
      * для ознакомления со списком документов чтобы взять питомца.
-     * Для проверки приюта происходит обращение к {@link UserRepository#findShelterTypeByChatId(long)}
      * @param chatId идентификатор чата, из которого пришел update
      */
     public void docsCommandReceived(long chatId) {
